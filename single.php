6be1d2d833a6d468a5aa35594a28a1f980aa7a8a
Brian Zalewski <TimeTravelersHackedMe@users.noreply.github.com>
2016-07-13 13:51:23 -0400

    Update single.php

--- single.php
+++ single.php
@@ -105,7 +105,7 @@ if ( ( ot_get_option( 'cb_postload_onoff', 'off' ) == 'off' ) && ( $cb_fis_size 
 							<?php }	?>
 							<?php 
 							$website_url = get_post_meta( $cb_post_id, 'website_url', true );
-							if(is_category('websites') || is_category('webapps')) {
+							if(is_category('Website Review') || is_category('Webapp Review')) {
 								echo $website_url;
 							}
 							?>
